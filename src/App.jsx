6aee451ecfe3db23b5b9e5fbb757d059603c5e39
Mohamad Sharafeddine <mhsharafeddine@gmail.com>
2024-07-25 21:15:14 +0300

    Added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./styles/App.css";
 import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <small>© 2024 Recipe Management System</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
